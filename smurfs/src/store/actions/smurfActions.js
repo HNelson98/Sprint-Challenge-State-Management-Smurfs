f01745ca2915e801bd23ac6de257eb4bc7fa3b6f
Henry Nelson <Henry.Nelson89@gmail.com>
2020-04-17 10:07:30 -0700

    met MVP and answered the questions

--- smurfs/src/store/actions/smurfActions.js
+++ smurfs/src/store/actions/smurfActions.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const FETCH_SMURF_START = 'FETCH_SMURF_START'
 export const FETCH_SMURF_SUCCESS = 'FETCH_SMURF_SUCCESS'
-export const FETCH_SMURF_FAILURE = 'FETCH_SMURF_FAILURE'
+
 
 export function fetchSmurf(){
     return dispatch => {
