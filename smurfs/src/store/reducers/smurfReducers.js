79422ff4deaa9a1721f8e757c680b950cb15cafd
Henry Nelson <Henry.Nelson89@gmail.com>
2020-04-17 08:25:31 -0700

    initalizing the project, and set up store and middleware

---
+++ smurfs/src/store/reducers/smurfReducers.js
@@ -0,0 +1,13 @@
+const initialState= {
+    name: "",
+    age: "",
+    height: "",
+
+}
+
+export const smurfReducer = (state = initialState, action) =>{
+    switch(action.type){
+        default:
+            return state
+    }
+}
